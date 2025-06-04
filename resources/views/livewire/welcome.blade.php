9ff775a7bbae9f04511a51155549c52cc1cc0831
Denis Augusto <denisgusto@gmail.com>
2025-06-04 15:17:59 -0300

    create welcome page

---
+++ resources/views/livewire/welcome.blade.php
@@ -0,0 +1,9 @@
+<div class="h-screen flex flex-col justify-center items-center p-6 bg-white">
+    <h1 class="text-3xl mb-4 tracking-wider">Seja bem-vindo! 🚀</h1>
+
+    <small class="text-gray-600">Você está no início de uma experiência incrível na utilização do filtro de produtos.</small>
+
+    <a href="{{ route('products.index') }}" class="rounded-md bg-indigo-600 px-3 py-2 mt-4 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        Começar a filtrar produtos
+    </a>
+</div>
